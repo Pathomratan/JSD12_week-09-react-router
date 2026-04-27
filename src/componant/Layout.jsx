26b5b9cbb9aa5ce35a0e5f47a0580e3326283af4
Pathomratan <Pathomrat.an@hotmail.com>
2026-04-27 15:04:28 +0700

    ีupdate product

--- src/componant/Layout.jsx
+++ src/componant/Layout.jsx
@@ -1,3 +1,12 @@
-export default function Layout () {
-    return <div>Navbar</div>
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <div className="bg-green-300">Navbar</div>
+      <div className="bg-purple-500">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
---
+++ src/mock-data/products.js
@@ -0,0 +1,10 @@
+export const products = [
+    {
+        id: "1",
+        name: "Product A",
+        description: "This is a function product",
+        price: 10.00,
+    },
+    { id: "2", name: "Product B", description: "This is a Beautiful product", price: 20.00 },
+    { id: "3", name: "Product C", description: "This is a great product", price: 30.00 },
+]
